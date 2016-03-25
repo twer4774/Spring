7e7b2f51bb710fb1b144a52d5d96d035bb3cb6f2
twer4774 <twer4774@gmail.com>
2016-03-25 18:47:10 +0900

    0325 Spring

---
+++ userDao/src/main/java/User.java
@@ -0,0 +1,32 @@
+/**
+ * Created by NCL on 2016-03-25.
+ */
+public class User {
+    private Long id;
+    private String name;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
