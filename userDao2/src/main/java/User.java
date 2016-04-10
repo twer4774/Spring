3c11045f2c4581dd63cc66a8a560d0361c6d0d67
twer4774 <twer4774@gmail.com>
2016-04-10 17:54:41 +0900

    add&&get

--- userDao2/src/main/java/User.java
+++ userDao2/src/main/java/User.java
@@ -2,13 +2,14 @@
  * Created by NCL on 2016-04-10.
  */
 public class User {
+
+    //유저를 위한 변수
     private Long id;
     private String name;
     private String password;
 
-    public Long getId() {
-        return id;
-    }
+    //get set 정의
+    public Long getId() {return id;}
 
     public void setId(Long id) {
         this.id = id;
