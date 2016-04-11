cd40c77189171113937b598435b1dd970e7c3213
twer4774 <twer4774@gmail.com>
2016-04-11 23:56:58 +0900

    SimpleConnectionMaker Create

--- userDao2/src/main/java/SimpleConnectionMaker.java
+++ userDao2/src/main/java/SimpleConnectionMaker.java
@@ -6,9 +6,8 @@ import java.sql.SQLException;
  * Created by NCL on 2016-04-11.
  */
 public class SimpleConnectionMaker {
-    public SimpleConnectionMaker(){
+    public SimpleConnectionMaker(){ //초기화를 위한 생성자
     }
-
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost/userinfo?characterEncoding=utf8","ncl","1234");
